49a08a85db52f9f1b5b1888b45590a5146acc8e4
TKaburu <traceymwendwa@gmail.com>
2024-06-13 11:14:17 +0300

    feat: Add User model for User Authentication Service

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+""" User Model """
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+
+class User(Base):
+    """
+    This class defines a user model
+
+    fields:
+        id, the integer primary key
+        email, a non-nullable string
+        hashed_password, a non-nullable string
+        session_id, a nullable string
+        reset_token, a nullable string
+    """
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250))
+    rest_token = Column(String(250))
